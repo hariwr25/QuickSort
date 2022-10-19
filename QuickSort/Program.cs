2fa1dc108fcd0d3135e96bc64e4359e742ca3bb1
HariWR <hariwr25@gmail.com>
2022-10-19 17:59:13 +0700

    menambahkan void display

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -103,5 +103,23 @@ namespace QuickSort
             //sort the list on the right of pivot using quick sort
             q_sort(j + 1, high);
         }
-       
+        void display()
+        {
+            Console.WriteLine("\n----------------------");
+            Console.WriteLine("\ Sorted array elements ");
+            Console.WriteLine("------------------------");
+
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine(arr[j]);
+            }
+            Console.WriteLine("\nNumber of comparision: " + cmp_count);
+            Console.WriteLine("\nNumber of data movements: " + mov_count);
+        }
+        int getSize()
+        {
+            return (n);
+        }
+
+        }
  
