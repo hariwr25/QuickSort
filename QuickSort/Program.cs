790ea3f95a9ea7eed87c52326c82e309df65ae8f
HariWR <hariwr25@gmail.com>
2022-10-19 16:48:37 +0700

    menambahlam number of element array

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -7,3 +7,12 @@ using System.Threading.Tasks;
 namespace QuickSort
 {
     class Program
+    {
+        //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; // number of comparasion
+        private int mov_count = 0; // number of data movement
+
+        // Number of element in array
+        private int n;
+    }
